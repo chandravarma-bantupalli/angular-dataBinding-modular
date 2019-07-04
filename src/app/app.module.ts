c21c2a9bc401ca01f6893801f338cea28b781dde
chandravarma-bantupalli <chandravarmabantupalli@gmail.com>
2019-07-05 00:19:19 +0530

    completed task 4: displaying child data in parent component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { Scenario1Module } from './scenario1/scenario1.module';
 import { Scenario2Module } from './scenario2/scenario2.module';
 import { Scenario3Module } from './scenario3/scenario3.module';
+import { Scenario4Module } from './scenario4/scenario4.module';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { Scenario3Module } from './scenario3/scenario3.module';
     AppRoutingModule,
     Scenario1Module,
     Scenario2Module,
-    Scenario3Module
+    Scenario3Module,
+    Scenario4Module
   ],
   providers: [],
   bootstrap: [AppComponent]
